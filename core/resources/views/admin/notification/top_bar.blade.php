ab54a62e301c3c19b22f9db0424da0b2069c80b2
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-09-21 23:33:11 +0500

    Fix notification top bar styling and purge legacy breadcrumb-nav white bg in app.css

--- core/resources/views/admin/notification/top_bar.blade.php
+++ core/resources/views/admin/notification/top_bar.blade.php
@@ -1,28 +1,27 @@
-<ul class="nav nav-tabs mb-4 topTap breadcrumb-nav" role="tablist">
-    <button class="breadcrumb-nav-close"><i class="las la-times"></i></button>
-    <li class="nav-item {{ menuActive(['admin.setting.notification.global.email','admin.setting.notification.global.sms','admin.setting.notification.global.push']) }}" role="presentation">
-        <a href="{{ route('admin.setting.notification.global.email') }}" class="nav-link text-dark" type="button">
-            <i class="las la-globe"></i> @lang('Global Template')
+<ul class="nav nav-tabs mb-4 topTap breadcrumb-nav border-0 d-flex gap-2" role="tablist">
+    <li class="nav-item" role="presentation">
+        <a href="{{ route('admin.setting.notification.global.email') }}" class="nav-link {{ menuActive(['admin.setting.notification.global.email','admin.setting.notification.global.sms','admin.setting.notification.global.push'], 2) ? 'active' : '' }}" type="button">
+            <i class="las la-globe me-1"></i> @lang('Global Template')
         </a>
     </li>
-    <li class="nav-item {{ menuActive('admin.setting.notification.email') }}" role="presentation">
-        <a href="{{ route('admin.setting.notification.email') }}" class="nav-link text-dark" type="button">
-            <i class="las la-envelope"></i> @lang('Email Setting')
+    <li class="nav-item" role="presentation">
+        <a href="{{ route('admin.setting.notification.email') }}" class="nav-link {{ menuActive('admin.setting.notification.email', 2) ? 'active' : '' }}" type="button">
+            <i class="las la-envelope me-1"></i> @lang('Email Setting')
         </a>
     </li>
-    <li class="nav-item {{ menuActive('admin.setting.notification.sms') }}" role="presentation">
-        <a href="{{ route('admin.setting.notification.sms') }}" class="nav-link text-dark" type="button">
-            <i class="las la-sms"></i> @lang('SMS Setting')
+    <li class="nav-item" role="presentation">
+        <a href="{{ route('admin.setting.notification.sms') }}" class="nav-link {{ menuActive('admin.setting.notification.sms', 2) ? 'active' : '' }}" type="button">
+            <i class="las la-sms me-1"></i> @lang('SMS Setting')
         </a>
     </li>
-    <li class="nav-item {{ menuActive('admin.setting.notification.push') }}" role="presentation">
-        <a href="{{ route('admin.setting.notification.push') }}" class="nav-link text-dark" type="button">
-            <i class="las la-bell"></i> @lang('Push Notification Setting')
+    <li class="nav-item" role="presentation">
+        <a href="{{ route('admin.setting.notification.push') }}" class="nav-link {{ menuActive('admin.setting.notification.push', 2) ? 'active' : '' }}" type="button">
+            <i class="las la-bell me-1"></i> @lang('Push Notification Setting')
         </a>
     </li>
-    <li class="nav-item {{ menuActive(['admin.setting.notification.templates','admin.setting.notification.template.edit']) }}" role="presentation">
-        <a href="{{ route('admin.setting.notification.templates') }}" class="nav-link text-dark" type="button">
-            <i class="las la-list"></i> @lang('Notification Templates')
+    <li class="nav-item" role="presentation">
+        <a href="{{ route('admin.setting.notification.templates') }}" class="nav-link {{ menuActive(['admin.setting.notification.templates','admin.setting.notification.template.edit'], 2) ? 'active' : '' }}" type="button">
+            <i class="las la-list me-1"></i> @lang('Notification Templates')
         </a>
     </li>
 </ul>
